af8d57e66b90a300f3f9dcd43cb5cb2390e45352
Eduardo Peruzzo <161074574+Eduardo-Peruzzo@users.noreply.github.com>
2024-03-06 10:07:59 -0300

    exercicios_ac.py

--- exercicios_ac.py
+++ exercicios_ac.py
@@ -9,8 +9,8 @@ a = float(input("Informe o parâmetro a da equação: "))
 b = float(input("Informe o parâmetro b da equação: "))
 c = float(input("Informe o parâmetro c da equação: "))
 
-x1 = (-b + (b**2 - 4*a*c)**(1/2)) / 2*a
-x2 = (-b - (b**2 - 4*a*c)**(1/2)) / 2*a
+x1 = (-b + (b**2 - 4*a*c)**(1/2)) / (2*a)
+x2 = (-b - (b**2 - 4*a*c)**(1/2)) / (2*a)
 
 print("A primeira raiz da equação é ", x1)
 print("A segunda raiz da equação é ", x2)
